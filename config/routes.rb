d73292d156df7d6bb0dfd6a11f8dbf1420e57adf
Olegs Osipovs <olegs.osipovs@gmail.com>
2025-02-06 14:55:18 +0200

    House controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "/houses/own", to: "houses#own"
+  resources :houses
+
   devise_for :users, controllers: {
     sessions: "sessions",
     registrations: "registrations"
